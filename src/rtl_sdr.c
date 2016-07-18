pinkavaj/rtl-sdr

rtl_sdr: units on -n

src/rtl_sdr.c
@@ -147,7 +147,7 @@ int main(int argc, char **argv)
 			out_block_size = (uint32_t)atof(optarg);
 			break;
 		case 'n':
-			bytes_to_read = (uint32_t)atof(optarg) * 2;
+			bytes_to_read = (uint32_t)atofs(optarg) * 2;
 			break;
 		case 'S':
 			sync_mode = 1;

rxseger/rx_tools

src/rtl_sdr.c
@@ -126,7 +126,7 @@ int main(int argc, char **argv)
 			break;
 		case 'n':
 			// half of I/Q pair count (double for one each of I and Q)
-			samples_to_read = (uint32_t)atof(optarg) * 2;
+			samples_to_read = (uint32_t)atofs(optarg) * 2;
 			break;
 		case 'S':
 			sync_mode = 1;