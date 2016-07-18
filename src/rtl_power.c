de21d9839e5836d01d4bd5e3f4df9405bc23cf71
rxseger <rseger@gmx.co.uk>
2016-07-17 22:52:52 -0700

    Revert "rtl_power: retune() flush buffer into same tuning_state buffer"
    This reverts commit ef612fb386ec049d0c456ae7f81e9461fdfd337f.

--- src/rtl_power.c
+++ src/rtl_power.c
@@ -530,8 +530,9 @@ void frequency_range(char *arg, double crop)
 	fprintf(stderr, "Buffer size: %i bytes (%0.2fms)\n", buf_len, 1000 * 0.5 * (float)buf_len / (float)bw_used);
 }
 
-void retune(SoapySDRDevice *d, SoapySDRStream *s, int freq, struct tuning_state *ts)
+void retune(SoapySDRDevice *d, SoapySDRStream *s, int freq)
 {
+	int16_t dump[BUFFER_DUMP];
 	int n_read;
 
 	SoapySDRKwargs args = {};
@@ -541,13 +542,13 @@ void retune(SoapySDRDevice *d, SoapySDRStream *s, int freq, struct tuning_state 
 	/* wait for settling and flush buffer */
 	usleep(5000);
 
-	void *buffs[] = {ts->buf16};
+	void *buffs[] = {dump};
 	int flags = 0;
 	long long timeNs = 0;
 	long timeoutNs = 1000000;
 	int r;
 
-	r = SoapySDRDevice_readStream(dev, stream, buffs, ts->buf_len, &flags, &timeNs, timeoutNs);
+	r = SoapySDRDevice_readStream(dev, stream, buffs, BUFFER_DUMP, &flags, &timeNs, timeoutNs);
 
 	if (r < 0) {
 		fprintf(stderr, "Error: bad retune.\n");}
@@ -656,7 +657,7 @@ void scanner(void)
 		f = (int32_t)SoapySDRDevice_getFrequency(dev, SOAPY_SDR_RX, 0);
 
 		if (f != ts->freq) {
-			retune(dev, stream, ts->freq, ts);}
+			retune(dev, stream, ts->freq);}
 
 		void *buffs[] = {ts->buf16};
 		int flags = 0;
