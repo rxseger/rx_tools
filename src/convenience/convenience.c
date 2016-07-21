b2316f38bd411e153a3b0500cd2eae035e73c017
Josh Blum <josh@joshknows.com>
2016-07-20 22:18:17 -0700

    provide strsep for parse_kwargs

--- src/convenience/convenience.c
+++ src/convenience/convenience.c
@@ -47,6 +47,18 @@ struct tm *localtime_r (const time_t *timer, struct tm *result)
     memcpy (result, local_result, sizeof (result));
     return result;
 }
+
+//http://unixpapa.com/incnote/string.html
+char * strsep(char **sp, char *sep)
+{
+    char *p, *s;
+    if (sp == NULL || *sp == NULL || **sp == '\0') return(NULL);
+    s = *sp;
+    p = s + strcspn(s, sep);
+    if (*p != '\0') *p++ = '\0';
+    *sp = p;
+    return(s);
+}
 #endif
 
 double atofs(char *s)
