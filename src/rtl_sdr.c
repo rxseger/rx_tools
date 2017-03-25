f52eb76320c0fa99f1e50d769b85392713827351
Matt Nottingham <matt.nottingham@zen.co.uk>
2017-03-25 09:28:30 +0000

    Add the channel aware long options.

--- src/rtl_sdr.c
+++ src/rtl_sdr.c
@@ -25,6 +25,7 @@
 
 #ifndef _WIN32
 #include <unistd.h>
+#include <getopt.h>
 #else
 #include <windows.h>
 #include <io.h>
@@ -40,6 +41,7 @@
 #define DEFAULT_BUF_LENGTH		(16 * 16384)
 #define MINIMAL_BUF_LENGTH		512
 #define MAXIMAL_BUF_LENGTH		(256 * 16384)
+#define MAX_NUM_CHANNELS                (8)
 
 static int do_exit = 0;
 static uint32_t samples_to_read = 0;
@@ -50,19 +52,25 @@ void usage(void)
 {
 	fprintf(stderr,
 		"rx_sdr (based on rtl_sdr), an I/Q recorder for RTL2832 based DVB-T receivers\n\n"
-		"Usage:\t -f frequency_to_tune_to [Hz]\n"
-		"\t[-s samplerate (default: 2048000 Hz)]\n"
+		"Usage:\t -f/--freq frequency_to_tune_to [Hz]\n"
+		"\t[-s/--srate samplerate (default: 2048000 Hz)]\n"
 		"\t[-d device key/value query (ex: 0, 1, driver=rtlsdr, driver=hackrf)]\n"
-		"\t[-g tuner gain(s) (ex: 20, 40, LNA=40,VGA=20,AMP=0)]\n"
+		"\t[-g/--gain tuner gain(s) (ex: 20, 40, LNA=40,VGA=20,AMP=0)]\n"
 		"\t[-p ppm_error (default: 0)]\n"
 		"\t[-b output_block_size (default: 16 * 16384)]\n"
 		"\t[-n number of samples to read (default: 0, infinite)]\n"
 		"\t[-F output format, CU8|CS8|CS16|CF32 (default: CU8)]\n"
 		"\t[-S force sync output (default: async)]\n"
 		"\t[-D direct_sampling_mode, 0 (default/off), 1 (I), 2 (Q), 3 (no-mod)]\n"
-		"\t[-A Name of antenna to use]\n"
+		"\t[-A/--Antenna ant   Name of antenna to use]\n"
+		"\t[-B/--Bandwidth bw  Receiver bandwidth to use]\n"
 		"\t[-N Number of receive channels to use]\n"
-		"\tfilename0 filename1 (a '-' dumps samples to stdout)\n\n");
+		"\tfilename0 [filename1.....] (a '-' dumps samples to stdout)\n"
+		"\n"
+		"\tAll long options have optional channel number, so --gain3 10 sets the\n"
+		"\tgain of channel 3 (starting from 0) to be '10'. -g/--gain would set the gain\n"
+		"\tof all the channels, for example.\n\n"
+		);
 	exit(1);
 }
 
@@ -90,45 +98,121 @@ int main(int argc, char **argv)
 #ifndef _WIN32
 	struct sigaction sigact;
 #endif
-	char *filename0 = NULL;
-        char *filename1 = NULL;
+	char *filename[MAX_NUM_CHANNELS];
 	int n_read;
 	int r, opt;
-	char *gain_str = NULL;
+	char *gain_str[MAX_NUM_CHANNELS];
 	int ppm_error = 0;
 	int sync_mode = 0;
 	int direct_sampling = 0;
-	FILE *file_ch0;
-        FILE *file_ch1;
-	int16_t *buffer_ch0;
-        int16_t *buffer_ch1;
-	double bw = -1;
+	FILE *file_ch[MAX_NUM_CHANNELS];
+	int16_t *buffer_ch[MAX_NUM_CHANNELS];
+	double bw[MAX_NUM_CHANNELS];
 	
 	uint8_t *buf8 = NULL;
 	float *fbuf = NULL; // assumed 32-bit
 	char *dev_query = NULL;
-	uint32_t frequency = 100000000;
-	uint32_t samp_rate = DEFAULT_SAMPLE_RATE;
+	uint32_t frequency[MAX_NUM_CHANNELS];
+	uint32_t samp_rate[MAX_NUM_CHANNELS];
 	uint32_t out_block_size = DEFAULT_BUF_LENGTH;
 	char *output_format = SOAPY_SDR_CU8;
-	char * ant = NULL;
+	char * ant[MAX_NUM_CHANNELS];
 	size_t * channels;
 	size_t nchan = 0;
 	size_t ch;
+	struct option *lg_opts = NULL;
+	char * base_opt[] = {"freq", "gain", "srate", "Ant", "Bandwidth"};
+	int o, count;
+	int option_idx;
 	
-	while ((opt = getopt(argc, argv, "d:f:g:s:b:n:p:D:SF:A:N:B:")) != -1) {
+	lg_opts = malloc(sizeof(struct option) * (MAX_NUM_CHANNELS * (sizeof(base_opt) / sizeof(char *) + 2)));
+	
+	for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+	  filename[ch] = NULL;
+	  gain_str[ch] = NULL;
+	  file_ch[ch] = NULL;
+	  buffer_ch[ch] = NULL;
+	  bw[ch] = -1;
+	  frequency[ch] = 40000000;
+	  samp_rate[ch] = DEFAULT_SAMPLE_RATE;
+	  ant[ch] = NULL;
+	}
+	
+	count = 0;
+	for (o = 0; o < sizeof(base_opt) / sizeof(char *); o++) {
+	  char * tmp_name;
+	  
+	  lg_opts[count].name = base_opt[o];
+	  lg_opts[count].has_arg = 1;
+	  lg_opts[count].flag = NULL;
+	  lg_opts[count].val = base_opt[0][0];
+	  count++;
+	  for(ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+	    tmp_name = malloc(strlen(base_opt[o]) + 10);
+	    if (MAX_NUM_CHANNELS > 9) {
+	      snprintf(tmp_name, strlen(base_opt[o]) + 10, "%s%02d", base_opt[o], ch);
+	    } else {
+	      snprintf(tmp_name, strlen(base_opt[o]) + 10, "%s%d", base_opt[o], ch);
+	    }
+	    lg_opts[count].name = tmp_name;
+	    lg_opts[count].has_arg = 1;
+	    lg_opts[count].flag = NULL;
+	    lg_opts[count].val = 0;
+	    count++;
+	  }
+	  
+	}
+	lg_opts[count].name = NULL;
+	lg_opts[count].has_arg = 0;
+	lg_opts[count].flag = NULL;
+	lg_opts[count].val = 0;
+	
+	while ((opt = getopt_long(argc, argv, "d:f:g:s:b:n:p:D:SF:A:N:B:", lg_opts, &option_idx)) != -1) {
 		switch (opt) {
+		case 0:
+		  printf("We got %s\n", lg_opts[option_idx].name);
+		  for (o = 0; o < sizeof(base_opt) / sizeof(char *); o++) {
+		    if (strncmp(base_opt[o], lg_opts[option_idx].name, strlen(base_opt[o])) == 0) {
+		      sscanf(&lg_opts[option_idx].name[strlen(base_opt[o])], "%d", &ch);
+		      printf("   for channel %d\n", ch);
+		      
+		      switch (base_opt[o][0]) {
+		      case 'f':
+			frequency[ch] = (uint32_t)atofs(optarg);
+			break;
+		      case 'g':
+			gain_str[ch] = optarg;
+			break;
+		      case 's':
+			samp_rate[ch] = (uint32_t)atofs(optarg);
+			break;
+		      case  'A':
+			ant[ch] = optarg;
+			break;
+		      case  'B':
+			bw[ch] = atofs(optarg);
+			break;
+		      }
+		    }
+		  }
+		  break;
 		case 'd':
 			dev_query = optarg;
 			break;
 		case 'f':
-			frequency = (uint32_t)atofs(optarg);
+		  	for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {			  
+			  frequency[ch] = (uint32_t)atofs(optarg);
+			}
 			break;
 		case 'g':
-			gain_str = optarg;
+		        for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+			  gain_str[ch] = optarg;
+			}
 			break;
 		case 's':
-			samp_rate = (uint32_t)atofs(optarg);
+		  for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+		    samp_rate[ch] = (uint32_t)atofs(optarg);
+		  }
 			break;
 		case 'p':
 			ppm_error = atoi(optarg);
@@ -144,10 +228,14 @@ int main(int argc, char **argv)
 			sync_mode = 1;
 			break;
 		case 'A':
-		        ant = optarg;
+		  for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+		        ant[ch] = optarg;
+		  }
 		        break;
 		case 'B':
-		        bw = atofs(optarg);
+		  for (ch = 0; ch < MAX_NUM_CHANNELS; ch++) {
+		    bw[ch] = atofs(optarg);
+		  }
 			break;
 		case 'N':
    		        nchan = atoi(optarg);
@@ -176,15 +264,12 @@ int main(int argc, char **argv)
 		}
 	}
 
-	if (argc <= optind) {
+	if (argc <= optind + nchan) {
 		usage();
 	} else {
-		filename0 = argv[optind];
-		if (argc > optind) {
-		  filename1 = argv[optind+1];
-		} else {
-		  filename1 = NULL;
-		}
+	  for (ch = 0; ch < nchan; ch++) {
+	    filename[ch] = argv[optind + ch];
+	  }
 	}
 
 	if(out_block_size < MINIMAL_BUF_LENGTH ||
@@ -198,9 +283,10 @@ int main(int argc, char **argv)
 		out_block_size = DEFAULT_BUF_LENGTH;
 	}
 
-	buffer_ch0 = malloc(out_block_size * SoapySDR_formatToSize(SOAPY_SDR_CS16));
-        buffer_ch1 = malloc(out_block_size * SoapySDR_formatToSize(SOAPY_SDR_CS16));
-
+	for(ch = 0; ch < nchan; ch++) {
+	  buffer_ch[ch] = malloc(out_block_size * SoapySDR_formatToSize(SOAPY_SDR_CS16));
+	}
+	
 	if (output_format == SOAPY_SDR_CS8 || output_format == SOAPY_SDR_CU8) {
 		buf8 = malloc(out_block_size * SoapySDR_formatToSize(SOAPY_SDR_CS8));
 	} else if (output_format == SOAPY_SDR_CF32) {
@@ -246,38 +332,41 @@ int main(int argc, char **argv)
 		verbose_direct_sampling(dev, direct_sampling);
 	}
 
-	if (bw < 0) {
-	  bw = 0.9 * samp_rate;
+	for(ch = 0; ch < nchan; ch++) {
+	    if (bw[ch] < 0) {
+	      bw[ch] = 0.9 * samp_rate[ch];
+	    }
 	}
+	
 	/* Set the sample rate */
 	/* Set the frequency */
 	/* Set the antenna */
 	/* Set the bandwidth */
 	if (channels == NULL) {
-   	        verbose_set_sample_rate(dev, samp_rate, 0);
-	        verbose_set_frequency(dev, frequency, 0);
-	        verbose_set_antenna(dev, ant, 0);
-		verbose_set_bandwidth(dev, bw, 0);
+   	        verbose_set_sample_rate(dev, samp_rate[0], 0);
+	        verbose_set_frequency(dev, frequency[0], 0);
+	        verbose_set_antenna(dev, ant[0], 0);
+		verbose_set_bandwidth(dev, bw[0], 0);
 	  	if (NULL == gain_str) {
 		        /* Enable automatic gain */
 		  verbose_auto_gain(dev, 0);
 		} else {
 		  /* Enable manual gain */
-		  verbose_gain_str_set(dev, gain_str, 0);
+		  verbose_gain_str_set(dev, gain_str[0], 0);
 		}
 		verbose_ppm_set(dev, ppm_error, 0);
 	} else {
    	       for(ch = 0; ch < nchan; ch++) {
-	           verbose_set_sample_rate(dev, samp_rate, channels[ch]);
-		   verbose_set_frequency(dev, frequency, channels[ch]);
-		   verbose_set_antenna(dev, ant, channels[ch]);
-		   verbose_set_bandwidth(dev, bw, channels[ch]);
+	           verbose_set_sample_rate(dev, samp_rate[ch], channels[ch]);
+		   verbose_set_frequency(dev, frequency[ch], channels[ch]);
+		   verbose_set_antenna(dev, ant[ch], channels[ch]);
+		   verbose_set_bandwidth(dev, bw[ch], channels[ch]);
 		   if (NULL == gain_str) {
 		     /* Enable automatic gain */
 		     verbose_auto_gain(dev, channels[ch]);
 		   } else {
 		     /* Enable manual gain */
-		     verbose_gain_str_set(dev, gain_str, channels[ch]);
+		     verbose_gain_str_set(dev, gain_str[ch], channels[ch]);
 		   }
 		   verbose_ppm_set(dev, ppm_error, channels[ch]);
 		   
@@ -285,29 +374,20 @@ int main(int argc, char **argv)
 	}
 	
 
-	if(strcmp(filename0, "-") == 0) { /* Write samples to stdout */
-		file_ch0 = stdout;
+	for(ch = 0; ch < nchan; ch++) {
+	  if(strcmp(filename[ch], "-") == 0) { /* Write samples to stdout */
+	    file_ch[ch] = stdout;
 #ifdef _WIN32
 		_setmode(_fileno(stdin), _O_BINARY);
 #endif
-	} else {
-		file_ch0 = fopen(filename0, "wb");
-		if (!file_ch0) {
-			fprintf(stderr, "Failed to open %s\n", filename0);
+	  } else {
+	    file_ch[ch] = fopen(filename[ch], "wb");
+		if (!file_ch[ch]) {
+			fprintf(stderr, "Failed to open %s\n", filename[ch]);
 			goto out;
 		}
-		if (filename1 != NULL) {
-		  file_ch1 = fopen(filename1, "wb");
-		  if (!file_ch1) {
-		    fprintf(stderr, "Failed to open %s\n", filename1);
-		    goto out;
-		  }
-		} else {
-		  file_ch1 = NULL;
-		}
-
+	  }
 	}
-
 	/* Reset endpoint before we start reading from it (mandatory) */
 	verbose_reset_buffer(dev);
 
@@ -320,13 +400,13 @@ int main(int argc, char **argv)
                 }
 		suppress_stdout_stop(tmp_stdout);
 		while (!do_exit) {
-			void *buffs[] = {buffer_ch0, buffer_ch1};
 			int flags = 0;
 			long long timeNs = 0;
 			long timeoutNs = 1000000;
 			int n_read = 0, r, i;
 
-			r = SoapySDRDevice_readStream(dev, stream, buffs, out_block_size, &flags, &timeNs, timeoutNs);
+			r = SoapySDRDevice_readStream(dev, stream, (void *) buffer_ch, out_block_size,
+						      &flags, &timeNs, timeoutNs);
 
 			//fprintf(stderr, "readStream ret=%d, flags=%d, timeNs=%lld\n", r, flags, timeNs);
 			if (r >= 0) {
@@ -347,46 +427,41 @@ int main(int argc, char **argv)
 			}
 
 			// TODO: read these formats natively from SoapySDR (setupStream) instead of converting ourselves?
-			if (output_format == SOAPY_SDR_CS16) {
-				// The "native" format we read in, write out no conversion needed
-				// (Always reading in CS16 to support >8-bit devices)
-				if (fwrite(buffer_ch0, sizeof(int16_t), n_read, file_ch0) != (size_t)n_read) {
-					fprintf(stderr, "Short write, samples lost, exiting!\n");
-					break;
-				}
-				if (file_ch1 != NULL) {
-				  if (fwrite(buffer_ch1, sizeof(int16_t), n_read, file_ch1) != (size_t)n_read) {
-				    fprintf(stderr, "Short write, samples lost, exiting!\n");
-				    break;
-				  }
-				}
+			for(ch = 0; ch < nchan; ch++) {
+			  if (output_format == SOAPY_SDR_CS16) {
+			    // The "native" format we read in, write out no conversion needed
+			    // (Always reading in CS16 to support >8-bit devices)
+			    if (fwrite(buffer_ch[ch], sizeof(int16_t), n_read, file_ch[ch]) != (size_t)n_read) {
+			      fprintf(stderr, "Short write, samples lost, exiting!\n");
+			      break;
+			    }
 
-			} else if (output_format == SOAPY_SDR_CS8) {
-				for (i = 0; i < n_read; ++i) {
-					buf8[i] = ( (int16_t)buffer_ch0[i] / 32767.0 * 128.0 + 0.4);
-				}
-				if (fwrite(buf8, sizeof(uint8_t), n_read, file_ch0) != (size_t)n_read) {
-					fprintf(stderr, "Short write, samples lost, exiting!\n");
-					break;
-				}
-			} else if (output_format == SOAPY_SDR_CU8) {
-				for (i = 0; i < n_read; ++i) {
-					buf8[i] = ( (int16_t)buffer_ch0[i] / 32767.0 * 128.0 + 127.4);
-				}
-				if (fwrite(buf8, sizeof(uint8_t), n_read, file_ch0) != (size_t)n_read) {
-					fprintf(stderr, "Short write, samples lost, exiting!\n");
-					break;
-				}
-			} else if (output_format == SOAPY_SDR_CF32) {
-				for (i = 0; i < n_read; ++i) {
-					fbuf[i] = buffer_ch0[i] * 1.0f / SHRT_MAX;
-				}
-				if (fwrite(fbuf, sizeof(float), n_read, file_ch0) != (size_t)n_read) {
-					fprintf(stderr, "Short write, samples lost, exiting!\n");
-					break;
-				}
+			  } else if (output_format == SOAPY_SDR_CS8) {
+			    for (i = 0; i < n_read; ++i) {
+			      buf8[i] = ( (int16_t)buffer_ch[ch][i] / 32767.0 * 128.0 + 0.4);
+			    }
+			    if (fwrite(buf8, sizeof(uint8_t), n_read, file_ch[ch]) != (size_t)n_read) {
+			      fprintf(stderr, "Short write, samples lost, exiting!\n");
+			      break;
+			    }
+			  } else if (output_format == SOAPY_SDR_CU8) {
+			    for (i = 0; i < n_read; ++i) {
+			      buf8[i] = ( (int16_t)buffer_ch[ch][i] / 32767.0 * 128.0 + 127.4);
+			    }
+			    if (fwrite(buf8, sizeof(uint8_t), n_read, file_ch[ch]) != (size_t)n_read) {
+			      fprintf(stderr, "Short write, samples lost, exiting!\n");
+			      break;
+			    }
+			  } else if (output_format == SOAPY_SDR_CF32) {
+			    for (i = 0; i < n_read; ++i) {
+			      fbuf[i] = buffer_ch[ch][i] * 1.0f / SHRT_MAX;
+			    }
+			    if (fwrite(fbuf, sizeof(float), n_read, file_ch[ch]) != (size_t)n_read) {
+			      fprintf(stderr, "Short write, samples lost, exiting!\n");
+			      break;
+			    }
+			  }
 			}
-
 
                         // TODO: hmm.. n_read 8192, but out_block_size (16 * 16384) is much larger TODO: loop? or accept 8192? rtl_fm ok with it
                         /*
@@ -406,17 +481,27 @@ int main(int argc, char **argv)
 	else
 		fprintf(stderr, "\nLibrary error %d, exiting...\n", r);
 
-	if (file_ch0 != stdout)
-		fclose(file_ch0);
-        if (file_ch1 != NULL) {
-	  fclose(file_ch1);
+	for(ch = 0; ch < nchan; ch++) {
+	  if (file_ch[ch] != stdout) {
+	    fclose(file_ch[ch]);
+	  }
 	}
 	
 	SoapySDRDevice_deactivateStream(dev, stream, 0, 0);
 	SoapySDRDevice_closeStream(dev, stream);
 	SoapySDRDevice_unmake(dev);
-	free (buffer_ch0);
-        free(buffer_ch1);
+
+	for(ch = 0; ch < nchan; ch++) {
+	  free(buffer_ch[ch]);
+	}
+
+	if (buf8 != NULL) {
+	  free(buf8);
+	}
+
+	if (fbuf != NULL) {
+	  free(fbuf);
+	}
 out:
 	return r >= 0 ? r : -r;
 }
